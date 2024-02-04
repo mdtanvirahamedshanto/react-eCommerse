3e29ef49e179f7c33976ed2300e08e2c08f9a71e
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-04 23:08:02 +0600

    Footer & Service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,44 @@
+import { GiReceiveMoney } from "react-icons/gi";
+import { MdSecurity } from "react-icons/md";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { TbTruckDelivery } from "react-icons/tb";
 import styled from "styled-components";
-
 const Services = () => {
   return (
     <>
       <Wrapper>
-        <div>Hello services</div>
+        <div className="container">
+          <div className="grid grid-three-column">
+            <div className="services-1">
+              <div>
+                <TbTruckDelivery className="icon" />
+                <h3>Super Fast and Free Delivery</h3>
+              </div>
+            </div>
+
+            <div className="services-2">
+              <div className="services-colum-2">
+                <div>
+                  <MdSecurity className="icon" />
+                  <h3>Non-contact Shipping</h3>
+                </div>
+              </div>
+              <div className="services-colum-2">
+                <div>
+                  <GiReceiveMoney className="icon" />
+                  <h3>Money-back Guaranteed</h3>
+                </div>
+              </div>
+            </div>
+
+            <div className="services-3">
+              <div>
+                <RiSecurePaymentLine className="icon" />
+                <h3>Super Secure Payment System</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </Wrapper>
     </>
   );
