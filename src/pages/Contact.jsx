9b97f9d8eb1bc0ad9c80c247c222c2e345c25bb1
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-05 11:44:00 +0600

    Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,60 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.5607610018515!2d90.24508887473742!3d21.87447195819647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30aa8bff3795beff%3A0x59a4acdac2163139!2sMd%20Tanvir%20Ahamed%20Shanto!5e0!3m2!1sen!2sbd!4v1707111028357!5m2!1sen!2sbd"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/moqgeley"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              // value=""
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              // value=""
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   padding: 9rem 0 5rem 0;
